b2f21e277d735728249aac9fe3c8877354724d21
Sheen Li <bingxun1101@gmail.com>
2019-11-27 17:01:23 +0800

    Add US

--- driving.py
+++ driving.py
@@ -3,6 +3,11 @@ age = int(input("你輸入年齡:"))
 if country == "台灣":
 	if age >= 18:
 		print("你可以開車")
-	else :
+	else:
+		print("你還不能開車")
+elif country == "美國":
+	if age >= 16:
+		print("你可以開車")
+	else:
 		print("你還不能開車")
 		
